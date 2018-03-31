e5e6272cc6ad92ee896aea1520a91d6c61a3aab0
Woxell <37600013+Woxell@users.noreply.github.com>
2018-03-31 21:43:05 +0200

    Update canvas.js

--- HTML-canvas-tutorial/canvas.js
+++ HTML-canvas-tutorial/canvas.js
@@ -61,10 +61,10 @@ function Circle(x, y, dx, dy, radius, r, g, b) {
 var circleArray = [];
 for (var i = 0; i < 400; i++) {
   var radius = 2;
-  var x = Math.random()*(innerWidth-radius*2)+radius;
-  var y = Math.random()*(innerHeight-radius*2)+radius;
-  var dx = (Math.random() - 0.5) * 50;
-  var dy = (Math.random() - 0.5) * 50;
+  var x = innerWidth/2;
+  var y = innerHeight/2;
+  var dx = Math.cos(Math.random() * Math.PI)*10;
+  var dy = Math.sin(Math.random() * Math.PI)*10;
   var r = Math.floor(Math.random() * 255);
   var g = Math.floor(Math.random() * 255);
   var b = Math.floor(Math.random() * 255);
