a883afeb98bd0909ccbf79be01badf91845a16b6
Woxell <37600013+Woxell@users.noreply.github.com>
2018-03-31 16:20:10 +0200

    Add files via upload

---
+++ HTML-canvas-tutorial/canvas.js
@@ -0,0 +1,98 @@
+var canvas = document.querySelector('canvas');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+canvas.background = 0;
+
+var c = canvas.getContext('2d');
+
+// c.fillStyle = 'rgba(255, 0, 0, 0.5)';
+// c.fillRect(100, 100, 100, 100);
+// c.fillStyle = 'rgba(0, 255, 0, 0.7)';
+// c.fillRect(210, 300, 150, 100);
+// c.fillStyle = 'rgba(0, 0, 255, 0.2)';
+// c.fillRect(250, 20, 100, 100);
+//
+// //Lines
+// c.beginPath();
+// c.moveTo(50, 300);
+// c.lineTo(300, 100);
+// c.lineTo(400,300);
+// c.strokeStyle = "yellow";
+//
+// c.stroke();
+
+//Arc / circle
+
+// for(var i = 0; i<10; i++)
+// {
+// 	var x = Math.random() * window.innerWidth;
+// 	var y = Math.random() * window.innerHeight;
+// 	c.beginPath();
+// 	c.arc(x,y,30, 0, Math.PI*2, false);
+// 	c.strokeStyle = "blue";
+// 	c.stroke();
+// }
+
+function Circle(x, y, dx, dy, radius){
+	this.x = x;
+	this.y = y;
+	this.dx = dx;
+	this.dy = dy;
+	this.radius = radius;
+
+	this.draw = function(){
+
+		c.beginPath();
+		c.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
+		c.strokeStyle = "Black";
+		c.stroke();
+	}
+
+	this.update = function(){
+		if(this.x+this.radius>window.innerWidth || this.x-this.radius<0)
+		{
+			this.dx = -this.dx;
+		}
+		if(this.y+this.radius>window.innerHeight || this.y-this.radius<0)
+		{
+			this.dy = -this.dy;
+		}
+
+		this.x+=this.dx;
+		this.y+=this.dy;
+	}
+}
+
+
+
+
+
+
+
+var circleArray = [];
+for(var i = 0; i<100; i++)
+{
+	var radius = 30;
+	var x = Math.random()*(innerWidth-radius)+radius;
+	var y = Math.random()*(innerHeight-radius)+radius;
+	var dx = (Math.random()-0.5)*5;
+	var dy = (Math.random()-0.5)*5;
+	circleArray.push(new Circle(x, y, dx, dy, radius));
+}
+
+
+function animate()
+{
+	requestAnimationFrame(animate);
+	c.clearRect(0, 0, innerWidth, innerHeight);
+	for(var i=0; i<circleArray.length; i++)
+	{
+		circleArray[i].draw();
+		circleArray[i].update();
+	}
+
+
+}
+
+animate();
